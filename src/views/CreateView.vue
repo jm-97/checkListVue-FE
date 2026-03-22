ecf82d2027db519db211493d6d8d57de386eaa0b
jose <manujose897@gmail.com>
2026-03-22 13:41:07 +0100

    Rename components

--- src/views/CreateView.vue
+++ src/views/CreateView.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
-import { reactive, watch } from 'vue';
-import TheWelcome from '../components/TheWelcome.vue'
-import type { Project } from '@/interfaces/projects';
+import { reactive, watch } from 'vue'
+import Creation from '../components/TheCreation.vue'
+import type { Project } from '@/interfaces/projects'
 const newPJ = reactive({
-  projectId: "",
-  name: ""
-});
-function updatePJ(pj:Project){
-
-}
-watch(newPJ, (nuovo)=>{
-  console.log("emitter", nuovo)
+  projectId: '',
+  name: '',
+})
+watch(newPJ, (nuovo) => {
+  console.log('emitter', nuovo)
 })
 </script>
 
 <template>
   <main>
-    <TheWelcome @newProject="(project) => Object.assign(newPJ, project)"/>
+    <Creation @newProject="(project: Project) => Object.assign(newPJ, project)" />
   </main>
 </template>
