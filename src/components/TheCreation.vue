23a32eb98644f8b1653769eb7823a4d3b8ae654a
jose <manujose897@gmail.com>
2026-07-03 17:33:03 +0200

    basic preview of template creation

--- src/components/TheCreation.vue
+++ src/components/TheCreation.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
+import { ACTIVITIES } from '@/assets/activities';
 import type { ProjectCreationDTO } from '@/interfaces/projects';
+import { computed, ref } from 'vue';
 
 
 const emit = defineEmits<{
   (e: "newProject", value: ProjectCreationDTO): void
 }>()
-let projectId = ''
-let name = ''
-let version = ''
+let projectId = ref('')
+let name = ref('')
+let version = ref('')
 function submit() {
   let id = crypto.randomUUID();
-  emit('newProject', { id, projectId, name, version } as ProjectCreationDTO)
+  emit('newProject', { id, projectId: projectId.value, name: name.value, version: version.value } as ProjectCreationDTO)
 }
+const DEFAULT_ACTIVITIES = computed(() => {
+  return ACTIVITIES(version.value, projectId.value);
+});
 </script>
 
 <template>
@@ -24,4 +29,8 @@ function submit() {
   <input type="text" v-model="version" name="version" id="version" placeholder="insert the version h.0.31" />
   <br />
   <button @click="submit">create project</button>
+
+  <div>
+    {{ DEFAULT_ACTIVITIES }}
+  </div>
 </template>
